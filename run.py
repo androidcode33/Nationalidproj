6d5f496c916ccaa10320024c8f4ab9f7f3bb282a
gmuhwezi <gmuhwezi@nwt.ug>
2017-11-13 12:25:21 -0800

    last changes

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'nationalid'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route("/")
 def index():
+  # conn = mysql.connect()
+  # cursor = conn.cursor()
+  # cursor.execute("SELECT * from User")
+  # data = cursor.fetchone()
   return render_template("index.html")
 
 @app.route("/about")
